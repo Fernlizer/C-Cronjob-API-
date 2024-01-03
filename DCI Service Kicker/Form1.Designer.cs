ccc3a2b60740a879978a5a659b7e8f11c5500085
Fernlizer <65641979+Fernlizer@users.noreply.github.com>
2024-01-03 10:50:48 +0700

    first commit with successfully

---
+++ DCI Service Kicker/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace DCI_Service_Kicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(341, 45);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cron Status :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Location = new System.Drawing.Point(71, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "OFF";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 512);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(341, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(51, 17);
+            this.toolStripStatusLabel1.Text = "CLOCK :";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(34, 17);
+            this.toolStripStatusLabel2.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(0, 312);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(341, 200);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(0, 152);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(341, 160);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(0, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 107);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(171, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 107);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Config File",
+            "Every 15 sec",
+            "Every 30 sec",
+            "Every 45 sec",
+            "Every 60 sec",
+            "Every 90 sec",
+            "Every 120 sec"});
+            this.comboBox1.Location = new System.Drawing.Point(214, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(341, 534);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DCi Service Kicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DCI Service Kicker/Form1.cs
@@ -0,0 +1,227 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Linq.Expressions;
+
+namespace DCI_Service_Kicker
+{
+    public partial class Form1 : Form
+    {
+        public List<ApiConfig> config; // Declare config as a class-level variable
+        public int every = 0;
+        public int SetEvery = 0;
+        public bool cron_every = false;
+        public bool cron = false;
+        public class ApiConfig
+        {
+            public string api { get; set; }
+            public string Bearer { get; set; }
+
+            public string time { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            // Define the Bangkok (Thailand) time zone
+            TimeZoneInfo bangkokTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+
+            // Get the current time in the Bangkok time zone
+            DateTime bangkokTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, bangkokTimeZone);
+
+            // Display the current time in the desired format
+            toolStripStatusLabel2.Text = bangkokTime.ToString("HH:mm:ss");
+
+            if (cron == true) {
+                foreach (var item in config)
+                {
+                    if (bangkokTime.ToString("HH:mm:ss") == item.time)
+                    {
+                        textBox2.AppendText("Cron of "+item.api+" Start");
+                        textBox2.AppendText(Environment.NewLine);
+                        await CallApiAsync(item); // Call the API request function when the condition is met
+                    }
+                    
+                }
+            }
+            if (cron_every ==  true)
+            {
+                label3.Text = every.ToString();
+                if (every == 0)
+                {
+                    foreach (var item in config)
+                    {
+                        await CallApiAsync(item); // Call the API request function when the condition is met
+
+                    }
+                    
+                    every = SetEvery;
+                }
+                every--;
+            }
+
+  
+        }
+        private async Task CallApiAsync(ApiConfig item)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Set the authorization header with the bearer token
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + item.Bearer);
+
+                try
+                {
+                    // Make a GET request to the API endpoint
+                    HttpResponseMessage response = await httpClient.GetAsync(item.api);
+
+                    // Check if the response is successful (status code 200)
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read and display the response content
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        textBox2.AppendText(Environment.NewLine);
+                        textBox2.AppendText("API Response: " + responseContent);
+                        textBox2.AppendText(Environment.NewLine);
+                    }
+                    else
+                    {
+                        textBox2.AppendText(Environment.NewLine);
+                        textBox2.AppendText("API Request Failed with status code: " + response.StatusCode);
+                        textBox2.AppendText(Environment.NewLine);
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText("API Request Failed with Exception: " + ex.Message);
+                    textBox2.AppendText(Environment.NewLine);
+                    
+                }
+            }
+            Thread.Sleep(1000);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filePath = "config.json"; // Replace with the path to your file
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("config.json does not exist.","config.json missing");
+                Application.Exit();
+                this.Close();
+                return;
+            }
+
+            timer1.Start();
+            // Read the JSON configuration from the file
+            string json = File.ReadAllText("config.json");
+
+            // Deserialize the JSON into a list of ApiConfig objects
+            config = JsonConvert.DeserializeObject<List<ApiConfig>>(json);
+
+            comboBox1.SelectedIndex = 0;
+
+            // Display the JSON data in textBox1 as multi-line text
+            if (config != null)
+            {
+                textBox1.Multiline = true;
+                textBox1.ScrollBars = ScrollBars.Both;
+                textBox1.WordWrap = false;
+
+                foreach (var item in config)
+                {
+                    if (item.Bearer == null) 
+                    {
+                        item.Bearer = "null";
+                    }
+                    textBox1.AppendText(item.api+" : "+ item.Bearer);
+                    textBox1.AppendText(Environment.NewLine); // Add a new line after each item
+                }
+            }
+            else
+            {
+                MessageBox.Show("No items in the JSON array.");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(config[0].api);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "Config File")
+            {
+                cron = true;
+                label2.Text = "ON";
+                textBox2.AppendText("CronJob Started with Config file.");
+                textBox2.AppendText(Environment.NewLine); // Add a new line after each item
+            }
+            else
+            {
+                textBox2.AppendText("CronJob Started with second tracking.");
+                textBox2.AppendText(Environment.NewLine); // Add a new line after each item
+                cron_every = true;
+                label2.Text = "ON";
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "Every 15 sec":
+                        SetEvery = 15;
+                        every = 15;
+                        break;
+                    case "Every 30 sec":
+                        SetEvery = 30;
+                        every = 30;
+                        break;
+                    case "Every 45 sec":
+                        SetEvery = 45;
+                        every = 45;
+                        break;
+                    case "Every 60 sec":
+                        SetEvery = 60;
+                        every = 60;
+                        break;
+                    case "Every 90 sec":
+                        SetEvery = 90;
+                        every = 90;
+                        break;
+                    case "Every 120 sec":
+                        SetEvery = 120;
+                        every = 120;
+                        break;
+                    default:
+                        SetEvery = 15;
+                        every = 15;
+                        break;
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            cron_every = false;
+            cron = false;
+            label2.Text = "OFF";
+            SetEvery = 0;
+            every = 0;
+            label3.Text = "";
+            textBox2.AppendText("CronJob Stoped.");
+            textBox2.AppendText(Environment.NewLine); // Add a new line after each item
+        }
+    }
+}
